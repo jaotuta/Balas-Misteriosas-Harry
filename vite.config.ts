c8f45a983f18cb56d621ed628a0843803a4a0008
João Lucas Cruz <63518059+jaotuta@users.noreply.github.com>
2025-12-30 14:19:51 +0000

    2

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,14 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, ".", "");
+  const isDev = mode === "development";
   return {
     server: {
       port: 3000,
       host: "0.0.0.0",
     },
     plugins: [react()],
-    base: "/",
+    base: isDev ? "/" : "/Balas-Misteriosas-Harry/",
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "."),
